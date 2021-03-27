f7cba9aa84cdf566331b433a4073993479a9c106
[nika-ribnitski] <nribn011@uottawa.ca>
2021-03-27 11:14:28 -0400

    Connection class comptete (for now hahaha)

--- src/connection.java
+++ src/connection.java
@@ -4,29 +4,41 @@ import java.util.Scanner;
 public class Connection { 
 		public static void main(String[] args) throws SQLException {
 
-			Scanner scanner = new Scanner(System.in);
-			System.out.println("\n" + "What Would You Like To Log In As? Type the number" + "\n");
-			System.out.println("1: Admin");
-	    	System.out.println("2: Employee");
-	    	System.out.println("3: Customer" + "\n");
-			String personType = scanner.nextLine();
+			String personType = "no one";
 
-			switch(personType) {
-				case ("1"):
-					personType = "Admin";
-					Admin admin = new Admin();
-					admin.AdminCase();
-					break;
-				case ("2"):
-					personType = "Employee";
-					Employee employee = new Employee("");
-					employee.EmployeeCase();
-					break;
-				case ("3"):
-					personType = "Customer";
-					Customer customer = new Customer("");
-					customer.CustomerCase();
-					break;
+			while (personType != "0"){ 
+				Scanner scanner = new Scanner(System.in);
+				System.out.println("\n" + "What Would You Like To Log In As? Type the number" + "\n");
+				System.out.println("1: Admin");
+				System.out.println("2: Employee");
+				System.out.println("3: Customer");
+				System.out.println("0: Exit" + "\n");
+				personType = scanner.nextLine();
+
+				switch(personType) {
+					case ("1"):
+						personType = "Admin";
+						Admin admin = new Admin();
+						admin.AdminCase();
+						break;
+					case ("2"):
+						personType = "Employee";
+						Employee employee = new Employee("");
+						employee.EmployeeCase();
+						break;
+					case ("3"):
+						personType = "Customer";
+						Customer customer = new Customer("");
+						customer.CustomerCase();
+						break;
+					case ("0"):
+						System.out.println("Thank you for using our service. Goodbye!");
+						System.exit(0);
+						break;
+					default:
+						System.out.println("Please enter a valid number.");
 					}
+
+			}
 		}
 	}
