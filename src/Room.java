bbcd2e17815025d007f3a60ce582642144d34a73
[nika-ribnitski] <nribn011@uottawa.ca>
2021-03-27 12:14:13 -0400

    created room search for availibitlity method

--- src/Room.java
+++ src/Room.java
@@ -8,7 +8,16 @@ import java.sql.Statement;
 import java.util.Scanner; 
 
 
-//this class will check room avaliability or we can do it within employee
+//this class will check room avaliability. It is a helper for employee.
 public class Room {
-    
+    //get start and end dates of stay in specific room
+    //compare them to booked and rented start/end dates in that room
+        //order rooms by arrival date, make sure ends don't overlap
+
+    public boolean roomFree (String start, String end, String room_num, String hotel_id){
+        //YYYY-MM-DD
+        
+        return true;
+
+    }  
 }
