96b07542d8ce967d4eba27502390c9b20b83292c
[nika-ribnitski] <nribn011@uottawa.ca>
2021-03-27 17:31:40 -0400

    Finished Room

--- src/Room.java
+++ src/Room.java
@@ -8,13 +8,14 @@ import java.sql.Statement;
 import java.util.Scanner; 
 
 
-//this class will check room avaliability. It is a helper for employee.
+//this class will check room openness. It is a helper for employee.
 public class Room {
-    private String[] start;
-    private String[] end;
+    private int[] start;
+    private int[] end;
     private String room_num;
     private String hotel_id;
-    private String date;
+    private int[] arrival;
+    private int[] departure;
     
     private Connection db;
     private Statement st; 
@@ -24,8 +25,8 @@ public class Room {
         //order rooms by arrival date, make sure ends don't overlap
 
     public Room(String start, String end, String room_num, String hotel_id){
-        this.start = start.split("-");
-        this.end = end.split("-");
+        this.start = dateToInt(start);
+        this.end = dateToInt(end);
         this.room_num = room_num;
         this.hotel_id = hotel_id;
     }
@@ -35,32 +36,24 @@ public class Room {
         Room room = new Room(start, end, room_num, hotel_id);
         try{
         //check booking
-            //SELECT * FROM booking WHERE room_num = this.room_num AND hotel_id = this.hotel_id ORDER BY arrival_date ASC;
-            //make sure the arrival date of an existing entry doesn't overlap the departure date of the new one
-            ResultSet rs = select("SELECT * FROM booking WHERE room_num = " + this.room_num + " AND hotel_id = " + this.hotel_id + " ORDER BY arrival_date ASC;");
-            while (rs.next()) {
-                date = rs.getString(4);
-                String[] dateArray = date.split("-"); //parse string into array [yyyy, mm, dd]
-                //compare to current    LOOKING FOR A FAIL!!!! SEARCH FOR WHEN IT DOESN'T WORK
-                if (this.start[0].equals(dateArray[0])){
-                    if (this.start[1].equals(dateArray[1])){
-                        int newDay = Integer.parseInt(this.start[2]);
-                        int oldDay = Integer.parseInt(date[2]);
-                        if ()
-                    }
+            ResultSet rsb = select("SELECT * FROM booking WHERE room_num = " + this.room_num + " AND hotel_id = " + this.hotel_id + " ORDER BY arrival_date ASC;");
+            while (rsb.next()) {
+                arrival = dateToInt(rsb.getString(4));	//int[] {yyyy, mm, dd}
+                departure = dateToInt(rsb.getString(5));
+                if (!room.compare(arrival, departure)) {	//compare to the dates in question
+                	return false;
                 }
             }
 
-            //SELECT * FROM booking WHERE room_num = this.room_num AND hotel_id = this.hotel_id ORDER BY departure_date ASC;
-            //make sure the departure date of an existing entry doesn't overlap the arrival date of the new one
-    
-
         //check renting
-            //SELECT * FROM renting WHERE room_num = this.room_num AND hotel_id = this.hotel_id ORDER BY arrival_date ASC;
-            //make sure the arrival date of an existing entry doesn't overlap the departure date of the new one
-            
-            //SELECT * FROM renting WHERE room_num = this.room_num AND hotel_id = this.hotel_id ORDER BY departure_date ASC;
-            //make sure the departure date of an existing entry doesn't overlap the arrival date of the new one
+            ResultSet rsr = select("SELECT * FROM renting WHERE room_num = " + this.room_num + " AND hotel_id = " + this.hotel_id + " ORDER BY arrival_date ASC;");
+            while (rsr.next()) {
+                arrival = dateToInt(rsr.getString(4));	//int[] {yyyy, mm, dd}
+                departure = dateToInt(rsr.getString(5));
+                if (!room.compare(arrival, departure)) {	//compare to the dates in question
+                	return false;
+                }
+             }
 
         } catch (SQLException e){
             System.out.println("--- Error get information from database");
@@ -75,5 +68,75 @@ public class Room {
         st = db.createStatement(); 
         ResultSet rs = st.executeQuery(query);
         return rs;
+    }
+    
+    public static int[] dateToInt(String date) {
+    	String[] tmp1 = date.split("-");	
+    	int day = Integer.parseInt(tmp1[2]);
+    	int mon = Integer.parseInt(tmp1[1]);
+    	int yea = Integer.parseInt(tmp1[0]);
+    	int[] ans = new int[] {yea, mon, day};
+    	return ans;
+    }
+    
+    
+    public boolean compare (int[] arrival, int[] departure) {
+    	if (arrival[0] != departure[0]) {	//when the years are different
+        	if ((arrival[0] + 2) <= departure[0] && this.start[0] > arrival[0] && this.start[0] < departure[0]) {
+        		return false;
+        	}
+        	if ((arrival[0] + 2) <= departure[0] && this.end[0] > arrival[0] && this.end[0] < departure[0]) {
+        		return false;
+        	}
+        	if (arrival[0] == this.start[0] && arrival[1] < this.start[1]) {
+        		return false;
+        	}
+        	if (arrival[0] == this.end[0] && arrival[1] < this.end[1]) {
+        		return false;
+        	}
+        	if (departure[0] == this.start[0] && departure[1] < this.start[1]) {
+        		return false;
+        	}
+        	if (departure[0] == this.end[0] && departure[1] < this.end[1]) {
+        		return false;
+        	} else {
+        		return true;
+        	}
+        }
+        if (arrival[0] == departure[0]) {	//when the years are the same
+        	if ((arrival[1] + 2) <= departure[1] && this.start[1] > arrival[1] && this.start[1] < departure[1]) {
+        		return false;
+        	}
+        	if ((arrival[1] + 2) <= departure[1] && this.end[1] > arrival[1] && this.end[1] < departure[1]) {
+        		return false;
+        	}
+        	//edge days
+        	if (arrival[1] != departure[1]) {
+        		if (arrival[1] == this.start[1] && arrival[2] < this.start[2]) {
+            		return false;
+            	}
+            	if (arrival[1] == this.end[1] && arrival[2] < this.end[2]) {
+            		return false;
+            	}
+            	if (departure[1] == this.start[1] && departure[2] < this.start[2]) {
+            		return false;
+            	}
+            	if (departure[1] == this.end[1] && departure[2] < this.end[2]) {
+            		return false;
+            	} else {
+            		return true;
+            	}
+        	}
+        	if (arrival[1] == departure[1]) {
+        		if (arrival[2] < this.start[2] && departure[2] > this.start[2]) {
+        			return false;
+        		}
+        		if (arrival[2] < this.end[2] && departure[2] > this.end[2]) {
+        			return false;
+        		}
+        	}
+        }
+        
+        return true; //if we didn't catch anything, it's probably fine :D
     }
 }
